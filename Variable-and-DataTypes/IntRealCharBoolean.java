fb6ea7f6b252a27e88a0e17afc131534abbc8417
Vansh52-coder <vanshlall52@gmail.com>
2026-06-10 00:08:43 +0530

    Add Initial Files

---
+++ Variable-and-DataTypes/IntRealCharBoolean.java
@@ -0,0 +1,25 @@
+public class IntRealCharBoolean {
+    public static void main (String[] args){
+        // Interger => byte , short , int , long
+        byte a = 120;
+        short b = 3000;
+        int c = 500000;
+        long d = 90000000;
+        
+        // real => float , double
+        float e = 3.14f;
+        double f = 5.456789;
+
+        // char => single character
+        char g = 'B';
+
+        // boolean => true or false
+        boolean h = false;
+
+        System.out.println("Integer values: " + a + " " + b + " " + c + " " + d);
+        System.out.println("Real values: " + e + " " + f);
+        System.out.println("Character value: " + g);
+        System.out.println("Boolean value: " + h);
+    
+    }
+}
