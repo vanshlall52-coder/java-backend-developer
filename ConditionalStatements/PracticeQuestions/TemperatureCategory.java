016cc272c70ac768a295d76b23b7ef92ea559aa5
Vansh52-coder <vanshlall52@gmail.com>
2026-06-18 16:57:25 +0530

    Update file

---
+++ ConditionalStatements/PracticeQuestions/TemperatureCategory.java
@@ -0,0 +1,12 @@
+package PracticeQuestions;
+import java.util.Scanner;
+public class TemperatureCategory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(" Enter the temperature");
+        int temp = scanner.nextInt();
+
+        
+
+    }
+}
